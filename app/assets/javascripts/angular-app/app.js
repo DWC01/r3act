96f7c211562e3185032b9698567bb7b0216ef13e
David Christian <david.w.christian@gmail.com>
2014-12-24 11:43:16 -0800

    angular starter app complete, with working API

---
+++ app/assets/javascripts/angular-app/app.js
@@ -0,0 +1,23 @@
+angular
+  
+  .module('r3act', [
+
+  ])
+
+  .controller('DashController', function($scope){
+    $scope.menuHeaders = [
+      {"id": 0, "header": "Summary"},
+      {"id": 1, "header": "Transactions"},
+      {"id": 2, "header": "Data"}
+    ];
+
+    $scope.currentHeader = null;
+
+    function setCurrentHeader(header) {
+      $scope.currentHeader = header;
+    }
+
+    $scope.setCurrentHeader = setCurrentHeader;
+  })
+
+

ffa7d06d5c18a94ef2ab29a9a8782f53a898e0b1
David Christian <david.w.christian@gmail.com>
2015-01-02 18:33:08 -0800

    cleared out all model, controller, app, files in angular, starting over to implement restangular"

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,43 +1,7 @@
 angular
-  
   .module('r3act', [
     'templates',
     'ui.router',
     'ngRoute', 
     'ngResource'
-  ])
-
-  .config(['$routeProvider', function($routeProvider){ 
-    $routeProvider.when('/',{
-      controller: 'CampaignsController'
-    }).when('/campaigns/:id',{
-      controller: 'CampaignsController',
-    }).otherwise({
-      redirectTo: '/'
-    });  
-  }]);
-  
-
-
-  // .config(function($stateProvider) {
-
-    // $stateProvider
-    //   .state('campaignHeader', {
-    //     url: '',
-    //     templateUrl: 'campaigns/campaignHeader.html',
-    //     controller: 'CampaignsController'
-    //   })
-    //   .state('placements', {
-    //     url: '',
-    //     templateUrl: 'placements.html',
-    //     controller: 'CampaignsController'
-    //   })
-    //   .state('audience', {
-    //     url: '',
-    //     templateUrl: 'audienceTargeting.html',
-    //     controller: 'CampaignsController'
-    //   })
-    // ;  
-  // })
-
-
+  ]);
--- app/assets/javascripts/angular-app/models/camapaignModel.js
+++ app/assets/javascripts/angular-app/models/camapaignModel.js
@@ -1,25 +1,2 @@
 angular
-  .module('r3act')
-
-  .factory('Campaign', function($resource) {
-    var Campaign = $resource('http://localhost:3000/api/campaigns/:id',
-    { id:'@id' }, {
-      'query': {
-        method: 'GET'
-      },
-      update: {
-        method: 'PUT'
-      }
-    });
-    return Campaign;
-  })
-
-  .factory('CampaignAdTag', function($resource) {
-    var CampaignAdTag = $resource('http://localhost:3000/api/campaigns/57/ad_tags',
-    { id:'@id' }, {
-      'query': {
-        method: 'GET'
-      }
-    });
-    return CampaignAdTag;
-  })
+  .module('r3act');

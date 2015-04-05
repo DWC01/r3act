0a3170a7968f3c81b5910c5f938ba6b2a729ff02
David Christian <david.w.christian@gmail.com>
2015-04-05 12:44:54 -0700

    Refactored buildFlights function in campaign.show route. Started building table for displaying AdTags.

--- frontend/app/routes/campaigns/show.js
+++ frontend/app/routes/campaigns/show.js
@@ -5,28 +5,28 @@ export default Ember.Route.extend({
     return this.store.find('campaign', params.campaign_id);
   },
   setupController: function (controller, model) {
+    var flights;
     controller.set('campaign', model);
-    controller.set('flights', this.flights(model));
 
-
-    var flights = controller.get('flights');
+    flights = this.buildFlights(controller, model);    
+    controller.set('flights', flights);
+  },
+  flights: function(model) {
+    return this.store.all('flight').filter(function(flight) {
+      return (String(flight.get('campaign_id'))) === model.id
+    });
+  },
+  buildFlights: function(controller, model) {
     var self = this;
+    var flights = this.flights(model);
 
     flights = flights.map(function(flight) {
       flight.set('tags', self.ad_tags(flight));
       return flight;
     });
-    
-    controller.set('flights', flights);
-  },
-  flights: function(model) {
-    // return this.store.find('flight', {campaign_id: model.id });
-    return this.store.all('flight').filter(function(flight) {
-      return (String(flight.get('campaign_id'))) === model.id
-    });
+    return flights;
   },
   ad_tags: function(flight) {
-    // return this.store.find('ad_tag', {flight_id: flight.id });
     return this.store.all('ad_tag').filter(function(ad_tag) {
       return (String(ad_tag.get('flight_id'))) === flight.get('id')
     });
